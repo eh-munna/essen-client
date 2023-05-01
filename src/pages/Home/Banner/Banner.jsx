6f2c99283d9e372eb131c84fda73526554f263ff
ehmunna <emran-hussain.munna-tu-ilmenau.de>
2023-05-02 00:32:58 +0200

    footer and banner design completed

--- src/pages/Home/Banner/Banner.jsx
+++ src/pages/Home/Banner/Banner.jsx
@@ -3,28 +3,24 @@ import chef from '../../../../public/Chef.png';
 
 const Banner = () => {
   return (
-    <div className="grid md:grid-cols-2 gap-3 items-center">
-      <div className="space-y-4">
-        <h1 className="md:text-5xl text-[#a5db67]">
-          <span className="text-[#398378]">Herzlich willkommen</span> <br /> in
-          unserer Küche
+    <div className="grid md:grid-cols-2 gap-3 items-center mt-4 md:mt-8">
+      <div className="order-2 md:order-1">
+        <h1 className="my-2 md:my-6 text-3xl md:text-5xl text-[#a5db67]">
+          <span className="text-[#398378]">Welcome</span> <br /> to our kitchen
         </h1>
 
-        <p>
-          Wählen Sie Ihr Essen von unseren besten Köchen. Wählen Sie Ihr Essen
-          von unseren besten Köchen. Wir sind die Beste in der Stadt.
-        </p>
+        <p className="my-2 md:my-6">Choose your food from our best chefs.</p>
+        <p>We provide best service as much as we can.</p>
 
         <button
           type="submit"
-          className="border p-1 border-[#8abe53] rounded-md text-[#fff] text-lg font-medium bg-[#8abe53]"
+          className="my-6 md:my-12 border p-1 border-[#8abe53] rounded-md text-[#fff] text-lg font-medium bg-[#8abe53]"
         >
-          Los geht&apos;s
+          Let&apos;s go
         </button>
       </div>
-      <div>
-        {' '}
-        <img className="max-w-full" src={chef} alt="" />{' '}
+      <div className="order-1 md:order-2">
+        <img className="max-w-full" src={chef} alt="" />
       </div>
     </div>
   );
--- src/shared/Footer/Footer.jsx
+++ src/shared/Footer/Footer.jsx
@@ -1,7 +1,68 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import {
+  FaFacebookF,
+  FaInstagram,
+  FaTwitter,
+  FaWhatsapp,
+} from 'react-icons/fa';
 
 const Footer = () => {
-  return <div>Here is our footer</div>;
+  return (
+    <div>
+      <div className="grid grid-cols-2 items-center justify-center">
+        <div>
+          <h4 className="font-medium text-[#398378] text-center my-2">
+            Follow Us
+          </h4>
+          <ul className="flex gap-3 justify-center items-center">
+            <li className="text-[#8abe53] text-xl">
+              <Link>
+                <FaFacebookF />
+              </Link>
+            </li>
+            <li className="text-[#8abe53] text-xl">
+              <Link>
+                <FaInstagram />
+              </Link>
+            </li>
+            <li className="text-[#8abe53] text-xl">
+              <Link>
+                <FaWhatsapp />
+              </Link>
+            </li>
+            <li className="text-[#8abe53] text-xl">
+              <Link>
+                <FaTwitter />
+              </Link>
+            </li>
+          </ul>
+        </div>
+        <div>
+          <h4 className="font-medium text-[#398378] text-center my-2">
+            Useful Links
+          </h4>
+          <ul className="flex flex-col justify-center items-center gap-3">
+            <li className="text-[#8abe53] text-base">
+              <Link to="/">Home</Link>
+            </li>
+            <li className="text-[#8abe53] text-base">
+              <Link to="/recipes">Recipes</Link>
+            </li>
+            <li className="text-[#8abe53] text-base">
+              <Link to="/sign-in">Sign In</Link>
+            </li>
+            <li className="text-[#8abe53] text-base">
+              <Link to="/registration">Sign Up</Link>
+            </li>
+          </ul>
+        </div>
+      </div>
+      <h6 className="text-center text-[#398378] my-4">
+        &copy;<Link to="/">Essen</Link> 2023
+      </h6>
+    </div>
+  );
 };
 
 export default Footer;
