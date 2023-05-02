c3cda58a865c3cfcc1bbb988de8ad80a438366c1
ehmunna <emran-hussain.munna-tu-ilmenau.de>
2023-05-02 16:59:51 +0200

    registration page and validation completed

--- src/pages/Registration/Registration.jsx
+++ src/pages/Registration/Registration.jsx
@@ -1,16 +1,52 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { AuthContext } from '../../providers/AuthProvider';
 
 const Registration = () => {
-  const { user } = useContext(AuthContext);
-  console.log(user);
+  const [success, setSuccess] = useState('');
+  const [error, setError] = useState('');
+  const { createUser } = useContext(AuthContext);
+
+  const userRegistration = (event) => {
+    event.preventDefault();
+    setSuccess('');
+    setError('');
+    const form = event.target;
+    const name = form.name.value;
+    const email = form.email.value;
+    const password = form.password.value;
+    const photo = form.photo.value;
+    if (password.length < 6) {
+      setError('Password must be or at least 6 characters long');
+      return;
+    }
+    if (email && password) {
+      createUser(email, password)
+        .then((result) => {
+          const createdUser = result.user;
+          form.reset();
+          setSuccess('User has been created successfully');
+        })
+        .catch((error) => {
+          const errorMessage = error.message;
+          // setError(errorMessage);
+        });
+    } else {
+      setError('Cannot register without providing email and password');
+      return;
+    }
+  };
+
   return (
     <div className="w-3/4 mx-auto py-3 md:py-10">
       <h2 className="my-4 text-center text-[#398378] text-2xl">
         Please Sign Up
       </h2>
-      <form className="w-full md:w-1/2 mx-auto">
+      <div className="text-center">
+        <h4 className="font-medium text-lg text-green-600">{success}</h4>
+        <h4 className="font-medium text-lg text-red-600">{error}</h4>
+      </div>
+      <form onSubmit={userRegistration} className="w-full md:w-1/2 mx-auto">
         <label htmlFor="email" className="block text-[#8ABE53] mt-4">
           Name
         </label>
@@ -18,14 +54,13 @@ const Registration = () => {
           required
           className="text-[#398378] w-full p-1 focus:outline-none border-b border-blue-800 focus:border-blue-500"
           type="text"
-          name="repeatPassword"
+          name="name"
           placeholder="Enter your name"
         />
         <label htmlFor="email" className="block text-[#8ABE53] mt-4">
           Email
         </label>
         <input
-          required
           className="text-[#398378] w-full p-1 focus:outline-none border-b border-blue-800 focus:border-blue-500"
           type="email"
           name="email"
@@ -35,7 +70,6 @@ const Registration = () => {
           Password
         </label>
         <input
-          required
           className="text-[#398378] w-full p-1 focus:outline-none border-b border-blue-800 focus:border-blue-500"
           type="password"
           name="password"
@@ -48,7 +82,7 @@ const Registration = () => {
           required
           className="text-[#398378] w-full p-1 focus:outline-none border-b border-blue-800 focus:border-blue-500"
           type="text"
-          name="repeatPassword"
+          name="photo"
           placeholder="Share your photo url"
         />
         <div className="my-4">
@@ -59,13 +93,13 @@ const Registration = () => {
             Sign Up
           </button>
         </div>
+        <h6 className="text-[#398378]  my-4 text-base">
+          Already have an account?{' '}
+          <Link className="text-[#8ABE53]" to="/sign-in">
+            Sign In
+          </Link>
+        </h6>
       </form>
-      <h6 className="text-[#398378]  my-4 text-base text-center">
-        Already have an account?{' '}
-        <Link className="text-[#8ABE53]" to="/sign-in">
-          Sign In
-        </Link>
-      </h6>
     </div>
   );
 };
