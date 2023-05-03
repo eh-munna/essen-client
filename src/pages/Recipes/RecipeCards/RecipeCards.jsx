7a9769fbf7552976115cb0025280d2a9d53ffe9c
ehmunna <emran-hussain.munna-tu-ilmenau.de>
2023-05-03 19:02:48 +0200

    button disabled and toast for bookmark done

--- src/pages/Recipes/RecipeCards/RecipeCards.jsx
+++ src/pages/Recipes/RecipeCards/RecipeCards.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
-
+import { Link } from 'react-router-dom';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 const RecipeCards = ({ recipe }) => {
   const { ingredients, name, method } = recipe;
   console.log(recipe);
@@ -16,6 +18,26 @@ const RecipeCards = ({ recipe }) => {
       </ul>
       <h5 className="my-2 font-medium">How to cook</h5>
       <p>{method}</p>
+      <div className="pt-6 rating">
+        <input
+          onClick={(event) => {
+            toast.success(`Recipe is bookmarked`, {
+              position: 'top-center',
+              hideProgressBar: false,
+              closeOnClick: true,
+              draggable: true,
+              progress: undefined,
+              theme: 'light',
+              autoClose: 1000,
+            });
+            event.currentTarget.disabled = true;
+          }}
+          type="radio"
+          name="rating-3"
+          className="disabled:opacity-30 disabled:cursor-default rating mask mask-heart bg-lime-400"
+        />
+      </div>
+      <ToastContainer />
     </div>
   );
 };
