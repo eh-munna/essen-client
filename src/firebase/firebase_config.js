86b8ad2f713500faaab1c5f137007f937e73c913
ehmunna <emran-hussain.munna-tu-ilmenau.de>
2023-05-04 18:26:01 +0200

    environment keys hidden

--- src/firebase/firebase_config.js
+++ src/firebase/firebase_config.js
@@ -3,14 +3,15 @@ import { initializeApp } from 'firebase/app';
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
+console.log('env variable', import.meta.env.VITE_apiKey);
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: 'AIzaSyCl0804kH3vFUkBclVbWqDt0PYwMlkkREY',
-  authDomain: 'chef-auth-6e24d.firebaseapp.com',
-  projectId: 'chef-auth-6e24d',
-  storageBucket: 'chef-auth-6e24d.appspot.com',
-  messagingSenderId: '61501541285',
-  appId: '1:61501541285:web:5f7afb9b433221e406364c',
+  apiKey: import.meta.env.VITE_APIKEY,
+  authDomain: import.meta.env.VITE_AUTHDOMAIN,
+  projectId: import.meta.env.VITE_PROJECTID,
+  storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
+  appId: import.meta.env.VITE_APPID,
 };
 
 // Initialize Firebase
